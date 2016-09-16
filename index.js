c7d7d21dfda266edeeab2c08a822919ad06953a2
ahyaku <ahyahyaku@gmail.com>
2016-09-16 16:38:04 +0900

    Move onKeyDown callback from index.js to mediate_pane.js.

--- index.js
+++ index.js
@@ -12,13 +12,7 @@ const async = require('async');
 
 const MediatePane = require('./lib/mediate_pane');
 
-let KEY_INPUT_MODE = {
-  NORMAL : 0,
-  SEARCH : 1,
-};
-
 let mediate_pane;
-let key_input_mode;
 let id_pane_cmd_left;
 let id_pane_cmd_right;
 
@@ -29,7 +23,7 @@ function init(){
   id_pane_cmd_left = document.getElementById('pane_cmd_left');
   id_pane_cmd_right = document.getElementById('pane_cmd_right');
 
-  document.addEventListener('keydown', onKeyDown);
+  //document.addEventListener('keydown', onKeyDown);
 
 
   //document.getElementById('pane_cmd_left').blur();
@@ -52,117 +46,6 @@ function init(){
   //                               'C:\\Go');
 
   mediate_pane.update;
-  key_input_mode = KEY_INPUT_MODE.NORMAL;
-
-}
-
-function onKeyDown(e){
-  //document.getElementById('pane_cmd_left').blur();
-  //document.getElementById('pane_cmd_right').blur();
-
-  //console.log("active: " + document.activeElement.id);
-  //console.log('key: ' + e);
-  //console.log('key_code: ' + e.keyCode);
-  //console.log('event.shiftKey: ' + event.shiftKey);
-  //console.log('event.ctrlKey: ' + event.ctrlKey);
-  //console.log('event.target: ' + event.target);
-  //console.log('event.target.id: ' + event.target.id);
-  switch(key_input_mode){
-    case KEY_INPUT_MODE.NORMAL:
-      checkKeyNormal(e);
-      break;
-    case KEY_INPUT_MODE.SEARCH:
-      checkKeySearch(e);
-      break;
-    default:
-      /* Do Nothing.. */
-      break;
-  }
-}
-
-function checkKeyNormal(e){
-  switch(e.keyCode){
-    case 72: /* 'h' */
-      mediate_pane.changeDirUpper();
-      break;
-    case 74: /* 'j' */
-      mediate_pane.cursorDown();
-      break;
-    case 75: /* 'k' */
-      mediate_pane.cursorUp();
-      break;
-    case 76: /* 'l' */
-      if(event.ctrlKey == true){
-        mediate_pane.updatePane();
-      }else{
-        mediate_pane.changeDirLower();
-      }
-      break;
-    case 9: /* 'tab' */
-      mediate_pane.switchPane();
-      break;
-    case 32: /* 'space' */
-      if(event.shiftKey == true){
-        mediate_pane.toggleUp();
-      }else{
-        mediate_pane.toggleDown();
-      }
-      break;
-    case 79: /* 'o' */
-      if(event.shiftKey == true){
-        mediate_pane.syncPaneOther2Cur();
-      }else{
-        mediate_pane.syncPaneCur2Other();
-      }
-      break;           
-    case 67: /* 'c' */
-      mediate_pane.copyItems();
-      break;           
-    case 68: /* 'd' */
-      mediate_pane.deleteItems();
-      break;           
-    case 77: /* 'm' */
-      break;           
-    case 81: /* 'q' */
-      mediate_pane.closeMainWindow();
-      break;           
-    case 188: /* ',' */
-      mediate_pane.showContextMenu();
-      break;           
-    case 13: /* 'enter' */
-      mediate_pane.openItem();
-      break;           
-    case 191: /* '/' */
-      key_input_mode = KEY_INPUT_MODE.SEARCH;
-      mediate_pane.startIsearch();
-      break;
-    default:
-      /* Do Nothing.. */
-      break;
-  }
-}
-
-function checkKeySearch(e){
-  switch(e.keyCode){
-    case 27:  /* 'ESC' */
-      key_input_mode = KEY_INPUT_MODE.NORMAL;
-      mediate_pane.endIsearch();
-      break;
-    case 219: /* '[' */
-      if(event.ctrlKey == true){
-        console.log('key: endIsearch!!');
-        key_input_mode = KEY_INPUT_MODE.NORMAL;
-        mediate_pane.endIsearch();
-      }
-      break;
-    case 9:  /* 'tab' */
-    case 13: /* 'enter */
-      e.preventDefault();
-      break;
-    default:
-      /* Do Nothing.. */
-      break;
-  }
 }
 
 init();
