fb2880dc71b54bb6917def0a45f90e6d7bd3cda7
ahyaku <ahyahyaku@gmail.com>
2016-09-16 23:59:32 +0900

    Fix the bug that cursor line is not displaied correctly.

--- index.js
+++ index.js
@@ -12,38 +12,15 @@ const async = require('async');
 
 const MediatePane = require('./lib/mediate_pane');
 
-let mediate_pane;
-let id_pane_cmd_left;
-let id_pane_cmd_right;
-
 function init(){
-  //let e = document.getElementById('window');
-  //e.addEventListener('keydown', onKeyDown);
-
-  id_pane_cmd_left = document.getElementById('pane_cmd_left');
-  id_pane_cmd_right = document.getElementById('pane_cmd_right');
-
-  //document.addEventListener('keydown', onKeyDown);
-
-
-  //document.getElementById('pane_cmd_left').blur();
-  //document.getElementById('pane_cmd_right').blur();
-
-  mediate_pane = new MediatePane('item_list_left', 
-                                 'item_list_right',
-                                 'dir_cur_left',
-                                 'dir_cur_right',
-                                 'pane_cmd_left',
-                                 'pane_cmd_right',
-                                 'C:\\Go',
-                                 'C:\\tmp\\test');
-
-  //mediate_pane = new MediatePane('items_left', 
-  //                               'items_right',
-  //                               'dir_cur_left',
-  //                               'dir_cur_right',
-  //                               'C:\\Go',
-  //                               'C:\\Go');
+  let mediate_pane = new MediatePane('item_list_left', 
+                                     'item_list_right',
+                                     'dir_cur_left',
+                                     'dir_cur_right',
+                                     'pane_cmd_left',
+                                     'pane_cmd_right',
+                                     'C:\\Go',
+                                     'C:\\tmp\\test');
 
   mediate_pane.update;
 }
