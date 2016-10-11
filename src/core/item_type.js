a920e9c295b3366cefb09c887abff4da29c30fe8
ahyaku <ahyahyaku@gmail.com>
2016-10-11 23:30:45 +0900

    Add necessary new src for item color setting.

---
+++ src/core/item_type.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const ITEM_TYPE_KIND = {
+  DIR      : 0,
+  TEXT     : 1,
+  EXE      : 2,
+  BIN      : 3,
+  IMAGE    : 4,
+  MOVIE    : 5,
+  SOUND    : 6,
+  ARCHIVE  : 7,
+  OTHER    : 8
+};
+
+export default ITEM_TYPE_KIND;
